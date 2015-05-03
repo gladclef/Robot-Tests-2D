b9a0989af0e9c3f305c31244c631cd32aefceaab
Benjamin Bean <benjamin.bean@comcast.net>
2015-05-02 20:14:10 -0600

    Finished physics calculations for segment

--- src/testsBodies/Segment.java
+++ src/testsBodies/Segment.java
@@ -29,23 +29,79 @@ public class Segment extends SegmentBody {
   }
   
   /**
-   * Calculates current torque on the joint.
+   * Calculates current torque on the joint as caused by gravity.
    */
   public float getTorque() {
+    
+    // get the center of mass and total mass
     Vec2 centerOfMass = new Vec2();
-    float totalMass = calculateLocalCenterOfMass(centerOfMass);
-    // TODO
-    return 0;
+    double totalMass = calculateLocalCenterOfMass(centerOfMass);
+    
+    // calculate the force caused by gravity at the center of mass
+    Vec2 grav = (Vec2) getBody().getWorld().getGravity();
+    double gravMagnitude = grav.length() * totalMass;
+    
+    // calculate the force against the normal to the vector describing
+    // the center of mass
+    double angle = Math.atan(centerOfMass.y / centerOfMass.x);
+    double x = 2 * gravMagnitude * Math.sin(angle / 2);
+    double y = Math.sqrt(gravMagnitude * gravMagnitude - x * x);
+    double normMagnitude = Math.sqrt(x * x + y * y);
+    
+    // calculate the torque
+    float torque = (float) (normMagnitude * centerOfMass.length());
+    double combinedAngle = getAngle(grav) - getAngle(centerOfMass);
+    if (Math.abs(combinedAngle) > Math.PI / 2) {
+        combinedAngle = - (Math.PI / 2 - Math.abs(combinedAngle));
+    }
+    float sign = (combinedAngle > 0) ? 1f : -1f; 
+    torque *= sign;
+    
+    return torque;
   }
   
   /**
    * Calculates the center of mass for the following segments, including the
-   *     end effector but not including this segment
+   *     end effector but not including this segment, relative to this
+   *     segments's joint position.
    * @param The center of mass
    * @return The total mass 
    */
   public float calculateLocalCenterOfMass(Vec2 centerOfMass) {
-    // TODO
-    return 0;
+    float totalMass = 0;
+    SegmentBody next = getNext();
+    
+    // handle case for end effector
+    if (next == null) {
+      return 0f;
+    }
+    
+    // add up the center of mass and total mass for each of the following
+    // segments
+    for (; next != null; next = next.getNext()) {
+      float mass = next.getBody().getMass();
+      totalMass += mass;
+      float massRatio = (totalMass == 0) ? (1f) : (mass / totalMass);
+      Vec2 nextWorldPos = (Vec2) next.getBody().getWorldCenter().sub(centerOfMass);
+      centerOfMass.addLocal(nextWorldPos.mul(massRatio));
+    }
+    
+    return totalMass;
+  }
+  
+  /**
+   * Gets the angle of the vector, from PI/2 to -PI/2,
+   *     where 0 is located at [1,0]
+   * @param v The vector
+   * @return Angle of the vector, in radians.
+   */
+  private float getAngle(Vec2 v) {
+    if (v.x == 0) {
+      if (v.y < 0) {
+        return (float) (-Math.PI / 4);
+      }
+      return (float) (Math.PI / 4);
+    }
+    return (float) Math.atan(v.y / v.x);
   }
 }
--- src/testsBodies/SegmentJUnit.java
+++ src/testsBodies/SegmentJUnit.java
@@ -5,12 +5,8 @@ package testsBodies;
 
 import static org.junit.Assert.*;
 
-import org.jbox2d.collision.shapes.CircleShape;
 import org.jbox2d.common.Vec2;
 import org.jbox2d.dynamics.Body;
-import org.jbox2d.dynamics.BodyDef;
-import org.jbox2d.dynamics.BodyType;
-import org.jbox2d.dynamics.FixtureDef;
 import org.jbox2d.dynamics.World;
 import org.junit.Test;
 
@@ -78,10 +74,11 @@ public class SegmentJUnit {
     Body body2 = MotorJUnit.getNewBody(world, new Vec2(2f, 0f));
     Segment segment = getNewSegment(world, new Vec2());
     segment.setNext(new Segment(1f, new Vec2(1f, 0f), 0f, body1));
-    segment.setNext(new Segment(1f, new Vec2(2f, 0f), 0f, body2));
+    segment.getNext().setNext(new Segment(1f, new Vec2(2f, 0f), 0f, body2));
     
     // verify the torque
-    float torque = body1.getMass() * 1f + body2.getMass() * 2f;
+    float grav = -1 * world.getGravity().length();
+    float torque = grav * (body1.getMass() * 1f + body2.getMass() * 2f);
     assertEquals(torque, segment.getTorque(), 0.01);
   }
 
@@ -95,7 +92,7 @@ public class SegmentJUnit {
     Body body2 = MotorJUnit.getNewBody(world, new Vec2(2f, 0f));
     Segment segment = getNewSegment(world, new Vec2());
     segment.setNext(new Segment(1f, new Vec2(1f, 0f), 0f, body1));
-    segment.setNext(new Segment(1f, new Vec2(2f, 0f), 0f, body2));
+    segment.getNext().setNext(new Segment(1f, new Vec2(2f, 0f), 0f, body2));
     
     // verify the center of mass and total mass
     Vec2 com = new Vec2();
